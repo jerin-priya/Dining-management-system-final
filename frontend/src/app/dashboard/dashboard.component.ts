0fc843a53251571d55e4a8aca494444325cdb2c0
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-10-28 19:29:37 +0600

    Dashboard Routing for User has been updated

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { DashboardService } from '../services/dashboard.service';
 import { SnackbarService } from '../services/snackbar.service';
 import { GlobalConstants } from '../shared/global-constants';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,18 +14,30 @@ import { GlobalConstants } from '../shared/global-constants';
 export class DashboardComponent implements AfterViewInit {
   responseMessage: any;
   data: any;
+  isAdmin: boolean = false; // Initialize isAdmin property
   ngAfterViewInit() {}
 
   constructor(
     private dashboardService: DashboardService,
     private ngxService: NgxUiLoaderService,
-    private snackBar: SnackbarService
+    private snackBar: SnackbarService,
+    private router: Router,
   ) {
     this.ngxService.start();
     this.dashboardData();
   }
 
   dashboardData() {
+    const token: string = localStorage.getItem('token')!;
+    let tokenPayload: any;
+    try {
+      tokenPayload = jwt_decode(token);
+      // Check if the user is an admin based on the role in the token
+      this.isAdmin = tokenPayload.role === 'admin';
+    } catch (error) {
+      localStorage.clear();
+      this.router.navigate(['/']);
+    }
     this.dashboardService.getDetails().subscribe(
       (resp: any) => {
         this.ngxService.stop();
